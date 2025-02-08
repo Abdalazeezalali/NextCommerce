3b12991f4c690500001eeb16494cc00d76507db9
Abdalazeezalali <abdalazeezalali387@gmail.com>
2025-02-08 21:15:25 +0300

    finish234

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -3,8 +3,8 @@ import { simplifiedProduct } from "../interface";
 import { client } from "../lib/sanity";
 import Image from "next/image";
 
-async function getData(cateogry: string) {
-  const query = `*[_type == "product" && category->name == "${cateogry}"] {
+async function getData(category: string) {
+  const query = `*[_type == "product" && category->name == "${category}"] {
         _id,
           "imageUrl": images[0].asset->url,
           price,
